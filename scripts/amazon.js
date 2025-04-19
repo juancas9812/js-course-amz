b78cc067444abf26f77658d806bc71cecc288a5e
juancas9812 <juancas9812@gmail.com>
2025-04-18 22:46:05 -0500

    Exercises 13a-13e

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -66,12 +66,17 @@ document.querySelectorAll('.js-add-to-cart')
         }
       });
 
+      const selectorItem = document.querySelector(`.js-quantity-selector-${productId}`);
+      let itemQuantity = Number(selectorItem.value);
+
       if (matchingItem) {
-        matchingItem.quantity+=1
+        // matchingItem.quantity+=1
+        matchingItem.quantity+=itemQuantity
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          // quantity: 1
+          quantity: itemQuantity
         })
       }
 
@@ -83,5 +88,6 @@ document.querySelectorAll('.js-add-to-cart')
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
+      // console.log(cart);
     });
   });
