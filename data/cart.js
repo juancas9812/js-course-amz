6124b0c74a0c63307e5bd80a2d4df9be41717bc6
juancas9812 <juancas9812@gmail.com>
2025-04-20 17:17:03 -0500

    Create automated test for add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId: '1'
-    },
-    {
-      productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId: '2'
-    }
-  ];
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: '1'
+      },
+      {
+        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: '2'
+      }
+    ];
+  }
 }
+
 
 
 function saveToStorage() {
@@ -30,7 +34,10 @@ export function addToCart(productId) {
   });
 
   const selectorItem = document.querySelector(`.js-quantity-selector-${productId}`);
-  let quantity = Number(selectorItem.value);
+  let quantity = 1;
+  if (selectorItem){
+    quantity = Number(selectorItem.value);
+  }
 
   if (matchingItem) {
     matchingItem.quantity+=quantity
